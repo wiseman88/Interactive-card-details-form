71c94f0f57ba99cc275852ffaa9f8d124fe2dc48
wiseman88 <wiseadam02@gmail.com>
2022-08-28 21:08:20 +0200

    two way binding added

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,28 @@
-console.log("works");
+// Form Handle
+
+const data = {
+  value: "",
+};
+
+Object.defineProperty(data, "prop", {
+  get: () => {
+    console.log("Getter called");
+    return this.value;
+  },
+  set: (value) => {
+    console.log("Setter called");
+    this.value = value;
+    printVal();
+  },
+});
+
+const el = document.getElementById("form-card-holder");
+
+el.addEventListener("keyup", (event) => {
+  data.prop = event.target.value;
+});
+
+function printVal() {
+  const el = document.getElementById("card-holder");
+  el.innerText = data.prop;
+}
